cd04a25e969d2f053ab9608b15a85c7d2a4ac054
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-10-03 19:40:30 -0300

    loadMovieDetails JS

--- script.js
+++ script.js
@@ -48,3 +48,18 @@ function displayMovieList(movies){
     }
     loadMovieDetails();
 }
+
+function loadMovieDetails(){
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            // console.log(movie.dataset.id);
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`);
+            const movieDetails = await result.json();
+            // console.log(movieDetails);
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
