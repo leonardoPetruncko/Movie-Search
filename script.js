d8e8da44de0e04de81d3bb25bf76cc35eb4159a2
Leonardo Petruncko <leonardopetruncko20@gmail.com>
2022-10-02 23:57:32 -0300

    JS api

---
+++ script.js
@@ -0,0 +1,7 @@
+// api https://omdbapi.com/?s=avengers&page=1&apikey=4b0e9f5
+
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// load movies from API
